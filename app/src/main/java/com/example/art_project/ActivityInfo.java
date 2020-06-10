f92cb05786e5fdf6e6ad962fb2bea5c48e2919fe
whiteformed <whiteformed@gmail.com>
2020-06-11 02:00:46 +0300

    minor adjustments.

--- app/src/main/java/com/example/art_project/ActivityInfo.java
+++ app/src/main/java/com/example/art_project/ActivityInfo.java
@@ -10,9 +10,23 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityInfo extends AppCompatActivity {
+    TextView tv_github;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        tv_github = findViewById(R.id.activity_info_tv_github);
+
+        View.OnClickListener onLinkClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/whiteformed/Debt-Manager"));
+                startActivity(intent);
+            }
+        };
+
+        tv_github.setOnClickListener(onLinkClickListener);
     }
 }
